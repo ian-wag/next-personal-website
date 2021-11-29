f9f648a9a4530f794ad2c5f9660894d3a0369ac3
iwagner76 <64107817+iwagner76@users.noreply.github.com>
2021-11-29 14:06:23 -0600

    added tailwind

---
+++ components/Header.js
@@ -0,0 +1,25 @@
+import Link from 'next/link';
+
+const Header = () => {
+  return (
+    <div>
+      <nav className="container m-auto grid grid-cols-3 bg-white items-center justify-center p-4">
+        <div className=" col-start-2 justify-self-center">
+          <Link href="/">
+            <a className="text-xl text-gray-800 font-semibold">Ian.</a>
+          </Link>
+        </div>
+        <div className="flex justify-self-end items-center">
+          <Link href="/work">
+            <a className="mr-6 lg:mr-10 text-blue-900 hover:text-indigo-600">work</a>
+          </Link>
+          <Link href="/info">
+            <a className=" text-blue-900 hover:text-indigo-600">info</a>
+          </Link>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Header;
